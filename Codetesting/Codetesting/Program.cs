6f18bc7685a28babe5d6ff1bae20ab77bb5416b1
xaenali <xaenali@gmail.com>
2017-11-03 18:54:14 +0100

    Intial layout

---
+++ Codetesting/Codetesting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codetesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             static public double permute(double[] focallength1, double[] focallength2, double[] focallength3)
+        {
+
+            int i, j, o ;           
+           
+            for (i = 0; i < focallength1.Length; i++)
+            {
+                for (j = 0; j < focallength2.Length; j++)
+                {
+                    for (o = 0; o < focallength3.Length; o++)
+                    {
+ double return = focallength1 + focallength2 + focallength3;
+                        return 0;
+                    }
+                }
+            }
+
+            
+        } 
+
+        }
+    }
+}
