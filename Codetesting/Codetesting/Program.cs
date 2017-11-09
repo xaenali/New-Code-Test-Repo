a6a51408983829ebb0d95beaaf89b8e23e92e7bd
xaenali <xaenali@gmail.com>
2017-11-09 19:17:08 +0100

    for loop

--- Codetesting/Codetesting/Program.cs
+++ Codetesting/Codetesting/Program.cs
@@ -1,43 +1,40 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CollectionApplication
 {
 
     class Program
     {
+        public static IList<double> focallength1 = new List<double>() { 130, 140, 150,140 ,1,2,4,5,5,65,6,6,6};
+        public static IList<double> focallength2 = new List<double>() { -19, -20, -21 };
+        public static IList<double> focallength3 = new List<double>() { 70, 71, 72 ,4,5,5,4,4,5,5,454,};
+        public static IList<double> a1 = new List<double>() { };
+
 
         static void Main(string[] args)
         {
-            ArrayList al = new ArrayList();
 
-            Console.WriteLine("Adding some numbers:");
-            al.Add(45);
-            al.Add(78);
-            al.Add(33);
-            al.Add(56);
-            al.Add(12);
-            al.Add(23);
-            al.Add(9);
 
-            Console.WriteLine("Capacity: {0} ", al.Capacity);
-            Console.WriteLine("Count: {0}", al.Count);
 
-            Console.Write("Content: ");
-            foreach (int i in al)
+            for (int i = 0; i < focallength1.Count; i++)
             {
-                Console.Write(i + " ");
+                for (int j = 0; j < focallength2.Count; j++)
+                {
+                    for (int k = 0; k < focallength3.Count; k++)
+                    {
+                        a1.Add(focallength1[i] + focallength2[j] + focallength3[k]);
+                        Console.WriteLine(a1[k]);
+
+                       // a1[k] = (double) F1[i] + F2[j] + F3[k];
+
+                    }
+                }
             }
 
-            Console.WriteLine();
-            Console.Write("Sorted Content: ");
-            al.Sort();
-            foreach (int i in al)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            Console.ReadKey();
         }
     }
 }
